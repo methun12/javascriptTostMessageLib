106af281218383a6252d654d8bdb9c4edab1f2a4
methun12 <69069999+methun12@users.noreply.github.com>
2022-09-10 00:07:31 +0600

    Update toastCreator.js

--- toastCreator.js
+++ toastCreator.js
@@ -8,48 +8,44 @@ let Toast=function(){
     idn=this.id;
     timex=this.duration;
     this.duration=duration;
-    		 this.styles = `
-    		 #`+this.id+`{
-    		 visibility: hidden;
-    		 min-width: 250px;
-    		 margin-left: -125px;
-    		 background-color: #333;
-    		 color: #fff;
-    		 text-align: center;
-    		 border-radius: 2px;
-    		 padding: 16px;
-    		 position: fixed;
-    		 z-index: 1;
-    		 bottom: 30px;
-    		 font-size: 17px;
-    		 }
-    		 
-    		 #`+this.id+`.show {
-    		 visibility: visible;
-    		 -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
-    		 animation: fadein 0.5s, fadeout 0.5s 2.5s;
-    		 }
-    		 
-    		 @-webkit-keyframes fadein {
-    		 from {bottom: 0; opacity: 0;} 
-    		 to {bottom: 30px; opacity: 1;}
-    		 }
-    		 
-    		 @keyframes fadein {
-    		 from {bottom: 0; opacity: 0;}
-    		 to {bottom: 30px; opacity: 1;}
-    		 }
-    		 
-    		 @-webkit-keyframes fadeout {
-    		 from {bottom: 30px; opacity: 1;} 
-    		 to {bottom: 0; opacity: 0;}
-    		 }
-    		 
-    		 @keyframes fadeout {
-    		 from {bottom: 30px; opacity: 1;}
-    		 to {bottom: 0; opacity: 0;}
-    		 }
-    		 `
+            this.styles=`
+		 #`+this.id+`{
+		 visibility: hidden;
+		 min-width: 250px;
+		 margin-left: -125px;
+		 background-color: #333;
+		 color: #fff;
+		 text-align: center;
+		 border-radius: 2px;
+		 padding: 16px;
+		 position: fixed;
+		 z-index: 1;
+		 left: 50%;
+		 bottom: 30px;
+		 font-size: 17px;
+		 }
+		 #`+this.id+`.show {
+		 visibility: visible;
+		 -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+		 animation: fadein 0.5s, fadeout 0.5s 2.5s;
+		 }
+		 @-webkit-keyframes fadein {
+		 from {bottom: 0; opacity: 0;} 
+		 to {bottom: 30px; opacity: 1;}
+		 }
+		 @keyframes fadein {
+		 from {bottom: 0; opacity: 0;}
+		 to {bottom: 30px; opacity: 1;}
+		 }
+		 @-webkit-keyframes fadeout {
+		 from {bottom: 30px; opacity: 1;} 
+		 to {bottom: 0; opacity: 0;}
+		 }
+		 @keyframes fadeout {
+		 from {bottom: 30px; opacity: 1;}
+		 to {bottom: 0; opacity: 0;}
+		 }
+		 `
     		 var styleSheet = document.createElement("style");
     		 styleSheet.innerText = this.styles
     		 document.head.appendChild(styleSheet)
